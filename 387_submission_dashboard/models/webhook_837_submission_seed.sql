5c756a488689861beddd70a1ba7d4df418c8eda6
Andy Chen <andy.chen@entyrecare.com>
2026-03-31 11:36:13 -0400

    filter for 837 production submissions

--- 387_submission_dashboard/models/webhook_837_submission_seed.sql
+++ 387_submission_dashboard/models/webhook_837_submission_seed.sql
@@ -1,16 +1,17 @@
 MODEL (
   name raw.stedi_837_submissions_seed,
-  kind SEED (
-    path '../19march26_837.csv'
-  ),
-  columns (
-    _created TIMESTAMP,
-    _id BIGINT,
-    _index INTEGER,
-    _ip TEXT,
-    _fivetran_synced TIMESTAMP,
-    headers TEXT,
-    data TEXT
-  ),
+  kind VIEW,
   grain (_id)
 );
+
+SELECT
+  _created,
+  _id,
+  _index,
+  _ip,
+  _fivetran_synced,
+  headers,
+  data
+FROM stedi_raw.events
+WHERE CAST(data AS JSONB) #>> '{event,detail,x12,metadata,transaction,transactionSetIdentifier}' = '837'
+  AND CAST(data AS JSONB) #>> '{event,detail,mode}' = 'production'
